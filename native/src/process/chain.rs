11e0637cc023155ed01d840768a20a7c38cfdaba
xiaoshuyui <528490652@qq.com>
2024-01-15 22:42:36 +0800

    Update chain.rs

--- native/src/process/chain.rs
+++ native/src/process/chain.rs
@@ -1,29 +1,54 @@
+use std::any::Any;
+
 pub trait Excute {
     fn excute(&self, d: &[u8]) -> Vec<u8>;
+
+    fn as_any(&self) -> &dyn Any;
 }
 
-pub struct Chain<T>
-where
-    T: Excute,
-{
-    pub encryptor: Vec<T>,
-    pub decryptor: Vec<T>,
+pub struct Chain {
+    pub encryptor: Vec<Box<dyn Excute>>,
+    pub decryptor: Vec<Box<dyn Excute>>,
     pub input: Vec<u8>,
     output: Vec<u8>,
 }
 
-impl<T> Chain<T>
-where
-    T: Excute,
-{
-    pub fn enctypt(&mut self) {
+impl Chain {
+    pub fn encrypt(&mut self) {
         if self.encryptor.is_empty() {
             self.output = self.input.clone();
             return;
         }
+        let mut my_vec: Vec<Box<dyn Excute>> = Vec::new();
+
+        /// TODO remove clone
+        let mut result = self.input.clone();
+
         for en in &self.encryptor {
-            self.output = en.excute(&self.input).to_vec();
+            if let Some(_xor) = en.as_any().downcast_ref::<XORExcutor>() {
+                my_vec.insert(
+                    0,
+                    Box::new(XORExcutor {
+                        key: _xor.key.clone(),
+                    }),
+                );
+            } else {
+                my_vec.insert(0, Box::new(NotExcutor {}));
+            }
+
+            result = en.excute(&result).to_vec();
         }
+        self.output = result;
+        self.decryptor = my_vec;
+    }
+
+    pub fn decrypt(&mut self) -> Vec<u8> {
+        /// TODO remove clone
+        let mut result = self.output.clone();
+        for en in &self.decryptor {
+            result = en.excute(&result).to_vec();
+        }
+        result
     }
 }
 
@@ -38,6 +63,22 @@ impl Excute for XORExcutor {
             .map(|(&x1, &x2)| x1 ^ x2)
             .collect::<Vec<u8>>()
     }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+pub struct NotExcutor {}
+
+impl Excute for NotExcutor {
+    fn excute(&self, d: &[u8]) -> Vec<u8> {
+        d.iter().map(|&x1| !x1).collect::<Vec<u8>>()
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 #[test]
@@ -46,14 +87,24 @@ fn test_chain() {
         key: "rfgbh12345".to_string().into_bytes(),
     };
 
+    let c2 = NotExcutor {};
+    let mut my_vec: Vec<Box<dyn Excute>> = Vec::new();
+    my_vec.push(Box::new(c));
+    my_vec.push(Box::new(c2));
     let mut chain = Chain {
-        encryptor: vec![c],
+        encryptor: my_vec,
         decryptor: Vec::new(),
         input: "abc123ghj6".to_string().into_bytes(),
         output: Vec::new(),
     };
 
-    chain.enctypt();
+    chain.encrypt();
 
-    println!("{:?}", chain.output) 
+    println!("{:?}", chain.output);
+
+    println!("len {:?}", chain.decryptor.len());
+
+    let result = chain.decrypt();
+
+    println!("{:?}", String::from_utf8(result));
 }
